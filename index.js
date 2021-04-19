3e34a32b8b872e14cc269a93524e48c2fd415ebe
Amanda <ajnikrant@gmail.com>
2021-04-19 17:35:54 -0400

    basic HTML structure

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // write JS code here
 
 logoPlacement = document.createElement("img")
+fetchAllListings()
 
 function fetchAllListings() {
     fetch('http://localhost:3000/listings')
-        .then(response => response.json)
+        .then(response => response.json())
         .then(listingsArr => {
             renderEachListing(listingsArr)
         })
     }
 
-fetchAllListings()
 
 function renderEachListing(listingsArr) {
     listingsArr.forEach(listingObj => {
-        const listingImg = document.querySelector("body > div > div > img")
-        listingImg.scr = listingObj.image
+        const listingImg = document.createElement("img")
+        listingImg.src = listingObj.image
+
+        const imageCard = document.querySelector("body > div > div")
+        imageCard.append(listingImg)
+    // console.log(listingObj)
     })
 
 }
