0a49c0039dd34880989a4be88ae284b789f887f4
Susana <susana.m.vik@gmail.com>
2021-04-19 16:29:40 -0400

    html and a bit of javascript

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // write JS code here
 
+logoPlacement = document.createElement("img")
+
+function fetchAllListings() {
+    fetch('http://localhost:3000/listings')
+        .then(response => response.json)
+        .then(listingsArr => {
+            renderEachListing(listingsArr)
+        })
+    }
+
+fetchAllListings()
+
+function renderEachListing(listingsArr) {
+    listingsArr.forEach(listingObj => {
+        const listingImg = document.querySelector("body > div > div > img")
+        listingImg.scr = listingObj.image
+    })
+
+}
