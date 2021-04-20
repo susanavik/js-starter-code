4c7fea4dd261a11c8e78963d0407cb5e9818b2bc
Radiyah <rsabur58@gmail.com>
2021-04-20 13:40:11 -0400

    Added some js functionality/shows listings in sidebar and can click/having issues with showing images

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
-// write JS code here
+/********** GLOABL VARIABLES **********/
 
 const logoPlacement = document.querySelector("#logo > img")
 logoPlacement.src = "assets/main-page-logo.png"
 
-fetchAllListings()
 
+
+/********** SHOWS LISTINGS IN SIDEBAR  **********/
 function fetchAllListings() {
     fetch('http://localhost:3000/listings')
         .then(response => response.json())
         .then(listingsArr => {
-            const listingImg = document.querySelector("#listing-details > div > img")
-            listingImg.src = listingsArr.image
+            listingsArr.forEach(listingObj => {
+                // console.log(listingObj)
+                renderEachListing(listingObj)
+            })
         })
-    }
-
-
-function renderEachListing(listingsArr) {
-    listingsArr.forEach(listingObj => {
-        const listingImg = document.querySelector("#listing-details > div > img")
-        listingImg.src = listingObj.image
-
-        const listingLocation = document.querySelector("#listing-details > div > div")
-        listingLocation.innerText = listingObj.location
-
-        // const listingPrice = document.querySelector("#listing-details > div > div > h5")
-        // listingPrice.innerText = listingObj.price
-    // console.log(listingObj)
-    })
-
 }
+
+function renderEachListing(listingObj) {
+
+    const listingLi = document.createElement('li.item')
+    listingLi.dataset.id = listingObj.id
+
+    listingLi.innerText = `
+    ${listingObj.name}
+    `
+
+    const allListingsContainer = document.querySelector('#side-bar > ul')
+    allListingsContainer.append(listingLi)
+}
+
+
+
+/********** CLICKED LISTING DETAILS  **********/
+function showListingDetailsHelper(listingObj) {
+    const listingImg = document.querySelector('#listing-details > div > img')
+    listingImg.src = listingObj.image
+    listingImg.alt = listingObj.name
+
+    const listingLocation = document.querySelector('#listing-details > div > div > h4')
+    listingLocation.innerText = listingObj.location
+
+    const listingPrice = document.querySelector('#listing-details > div > div > h4')
+    listingPrice.innerText = `$${listingObj.price}`
+
+    const listingDesc = document.querySelector('#listing-details > h4')
+    listingDesc.innerText = `Description: ${listingObj.description}`
+}
+
+function showListingDetails() {
+    const listingContainer = document.querySelector('#side-bar')
+
+    listingContainer.addEventListener('click', event => {
+        console.log(event.target.dataset.id)
+
+        if (event.target.dataset.id)
+            fetch(`http://localhost:3000/listings/${event.target.dataset.id}`)
+                .then(resp => resp.json())
+                .then(singleListing => {
+                    showListingDetailsHelper(singleListing)
+                })
+    })
+}
+
+
+
+
+/********** APP INIT **********/
+fetchAllListings()
+showListingDetails()
